bd7cee0eaa45111d9138834c4ebc7a11a0f2b677
Jonnier <alzatejonny39@gmail.com>
2024-10-27 15:24:01 -0500

    Mejoras

---
+++ js/administrador.js
@@ -0,0 +1,28 @@
+const showHiddenPassword = (inputPassword, inputIcon) => {
+    const input = document.getElementById(inputPassword),
+          iconEye = document.getElementById(inputIcon)
+  
+    iconEye.addEventListener('click', () => {
+      if (input.type === 'password') {
+        input.type = 'text';
+        iconEye.classList.add('ri-eye-line');
+        iconEye.classList.remove('ri-eye-off-line');
+      } else {
+        input.type = 'password';
+        iconEye.classList.remove('ri-eye-line');
+        iconEye.classList.add('ri-eye-off-line');
+      }
+    });
+  }
+  showHiddenPassword('password', 'input-icon');
+  document.querySelector('.login__form').addEventListener('submit', (e) => {
+    e.preventDefault(); 
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    if (email === 'admin444@gmail.com' && password === 'TecnoTapiz') {
+      window.location.href = 'carrito.html'; 
+    } else {
+      alert('Correo o contraseña incorrectos');
+    }
+  });
+  
