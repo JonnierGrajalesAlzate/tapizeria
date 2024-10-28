73dc536bad44f412ef1e719279f4ed2eacc8749a
JonniDevs <105441441+JonnierGrajalesAlzate@users.noreply.github.com>
2024-10-27 23:44:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const db = new sqlite3.Database('./catalogo_sofas.db'); // Asegúrate de que la ruta sea correcta
+const db = new sqlite3.Database('./catalogo_sofas.db'); 
 
-app.use(cors()); // Permitir CORS
-app.use(bodyParser.json()); // Middleware para parsear JSON
-app.use(express.static('public')); // Servir archivos estáticos desde la carpeta 'public'
-
-// Crear un Producto (POST)
+app.use(cors()); 
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.post('/productos', (req, res) => {
     const { titulo, imagen, precio } = req.body;
     db.run(`INSERT INTO productos (titulo, imagen, precio) VALUES (?, ?, ?)`, [titulo, imagen, precio], function(err) {
@@ -22,7 +20,7 @@ app.post('/productos', (req, res) => {
     });
 });
 
-// Leer Productos (GET)
+
 app.get('/productos', (req, res) => {
     db.all('SELECT * FROM productos', [], (err, rows) => {
         if (err) {
@@ -32,7 +30,6 @@ app.get('/productos', (req, res) => {
     });
 });
 
-// Inicializar el servidor
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
