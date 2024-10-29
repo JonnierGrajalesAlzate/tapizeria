6d1146850b4655897e61e77c22a8d2fb64a8bd6a
Jonnier <alzatejonny39@gmail.com>
2024-10-29 16:24:41 -0500

    Correcciones

--- js/formulario.js
+++ js/formulario.js
@@ -1,3 +1,3 @@
 document.getElementById("enviarBtn").addEventListener("click", function() {
-    alert("Muchas Gracias por llenar este formulario, pronto nos contactaremos con usted.");
+    alert("Muchas Gracias por llenar este formulario, Su cita quedo agendada correctamente.");
 });
--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+
 const app = express();
 const db = new sqlite3.Database('./catalogo_sofas.db'); 
 
-app.use(cors()); // Permitir CORS
-app.use(bodyParser.json()); // Middleware para parsear JSON
-app.use(express.static('public')); // Servir archivos estáticos desde la carpeta 'public'
-
-// Crear un Producto (POST)
+app.use(cors()); 
+app.use(bodyParser.json()); 
+app.use(express.static('public')); 
 app.post('/productos', (req, res) => {
     const { titulo, imagen, precio } = req.body;
     db.run(`INSERT INTO productos (titulo, imagen, precio) VALUES (?, ?, ?)`, [titulo, imagen, precio], function(err) {
@@ -21,7 +20,6 @@ app.post('/productos', (req, res) => {
     });
 });
 
-// Leer Productos (GET)
 app.get('/productos', (req, res) => {
     db.all('SELECT * FROM productos', [], (err, rows) => {
         if (err) {
@@ -30,8 +28,28 @@ app.get('/productos', (req, res) => {
         res.json(rows);
     });
 });
+app.put('/productos/:id', (req, res) => {
+    const { id } = req.params;
+    const { titulo, imagen, precio } = req.body;
 
-// Inicializar el servidor
+    db.run(`UPDATE productos SET titulo = ?, imagen = ?, precio = ? WHERE id = ?`, [titulo, imagen, precio, id], function(err) {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+        res.status(200).json({ changes: this.changes });
+    });
+});
+
+app.delete('/productos/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.run(`DELETE FROM productos WHERE id = ?`, [id], function(err) {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+        res.status(200).json({ changes: this.changes });
+    });
+});
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
